59175caf2567801df279248e67a03a7755501204
Andrew <gorbanevaa@gmail.com>
2020-05-18 11:58:13 +0300

    stuff

--- tasks/unit3_lesson6_step3.py
+++ tasks/unit3_lesson6_step3.py
@@ -3,24 +3,42 @@ import pytest
 import time
 import math
 
-def calc(x):
-      return str (math.log(int(time.time())))
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def browser():
     print("\nSTART")
     browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
     yield browser
     print("\nSTOP")
-    time.sleep(10)
+    time.sleep(2)
     browser.quit()
 
+@pytest.mark.parametrize('link', ["236895", "236896", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
+
 class TestAllienPage():
-    def test_allien_link1(self, browser):
-        browser.get("https://stepik.org/lesson/236895/step/1")
-        time.sleep(10)
-        x = calc
-        input1 = browser.find_element_by_id("ember68")
-        input1.send_keys(calc(x))
-        button = browser.find_element_by_class("submit-submission")
+    def test_allien_link1(self, browser, link): 
+        link = f"https://stepik.org/lesson/{link}/step/1"
+        browser.get(link)
+        answer = str (math.log(int(time.time())))
+        input1 = browser.find_element_by_css_selector("textarea.textarea")
+        input1.send_keys(answer)
+        button = browser.find_element_by_css_selector("button.submit-submission")
         button.click()
+        x = browser.find_element_by_css_selector(".smart-hints__hint")
+        #print (x.text)
+        assert x.text == "Correct!", "Test failed"
+      
+        
+        
+       
+        
+        
+        
+        #x = browser.find_element_by_css_selector("pre.smart-hints__hint")
+        
+        #y = x.get_attribute("text")
+        #print(y. text)
+       # z = "Correct!"
+        # assert y == z, "Test failed"
+        #assert correct_txt == correct_txt1, "Test failed"
